de2882a1b1b7692dd93b6c4b9dec16956fc97dbd
regno01 <212147545+regno001@users.noreply.github.com>
2026-01-09 23:46:00 +0530

    functionality

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-let boxes = document.querySelector(".box");
+let boxes = document.querySelectorAll(".box");
 
 let resetBtn = document.querySelector(".reset");
 
+let turnO = true;
+ 
+const winPAtterns = [
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [3,4,5],
+  [6,7,8],
+];
+
+
+boxes.forEach((box)=>{
+  box.addEventListener("click",()=>{
+  if(turnO){
+    box.innerText="O";
+    turnO = false;
+  }else{
+  box.innerText="X";
+    turnO = true;
+ 
+  }
+  box.disabled=true;
+  });
+});
 
