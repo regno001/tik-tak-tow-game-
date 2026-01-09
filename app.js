263df90b2b92dd3804ec8ca6943a876d01b23a42
regno01 <212147545+regno001@users.noreply.github.com>
2026-01-10 00:14:09 +0530

    working model

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ let boxes = document.querySelectorAll(".box");
 
 let resetBtn = document.querySelector(".reset");
 
+let msgContainer = document.querySelector(".msg-Container");
+let msg = document.querySelector(".msg");
+let newBTn = document.querySelector("#newBtn")
+
 let turnO = true;
  
 const winPAtterns = [
@@ -27,6 +31,26 @@ boxes.forEach((box)=>{
  
   }
   box.disabled=true;
+  checkWinner();
   });
 });
+
+const showWinner =(winner) => {
+msg.innerText = `Congratulation , winner is ${winner}`;
+msgContainer.classList.remove("hide");
+}
+
+
+const checkWinner= ()=>{
+for (let pattern of winPAtterns){
+  let pos1Val = boxes[pattern[0]].innerText;
+  let pos2Val = boxes[pattern[1]].innerText;
+  let pos3Val = boxes[pattern[2]].innerText;
+  if(pos1Val != "" && pos2Val != "" && pos3Val!= ""){
+    if(pos1Val === pos2Val && pos2Val === pos3Val){
+      showWinner(pos1Val);
+    }
+  }
+}
+}
 
